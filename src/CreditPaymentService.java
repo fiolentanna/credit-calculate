c52fb0a9ddfc47efd7d4cbb326a1b6d22c9b3d00
fiolentanna <tigranka@gmail.com>
2021-04-13 23:29:40 +0300

    chsnges 2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,11 @@
 public class CreditPaymentService {
-    public  double  calculate (int creditAmount, int creditTime, double creditRate) {
+    public double calculate(int creditAmount, int creditTime, double creditRate) {
         double monthCoeff = creditRate / 100 / 12;
         int numbersOfPayment = 12 * creditTime;
         double r = Math.pow(1 + monthCoeff, numbersOfPayment);
 
-        return  creditAmount * ((monthCoeff * r) / (r - 1));
+        return creditAmount * ((monthCoeff * r) / (r - 1));
     }
 
 }
+git
