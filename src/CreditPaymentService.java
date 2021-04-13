3f5518fe0b706b34eea410b104f1db2bc978ee64
fiolentanna <tigranka@gmail.com>
2021-04-13 23:28:30 +0300

    changes

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,10 @@
 public class CreditPaymentService {
-    public double calculate(int creditAmount, int creditTime, double annuityCoeff) {
-        double monthCoeff = annuityCoeff/100/12;
+    public  double  calculate (int creditAmount, int creditTime, double creditRate) {
+        double monthCoeff = creditRate / 100 / 12;
         int numbersOfPayment = 12 * creditTime;
+        double r = Math.pow(1 + monthCoeff, numbersOfPayment);
 
-        return creditAmount * (monthCoeff * Math.pow(1 + monthCoeff, numbersOfPayment) / (Math.pow(1 + monthCoeff, numbersOfPayment) - 1));
+        return  creditAmount * ((monthCoeff * r) / (r - 1));
     }
 
 }
